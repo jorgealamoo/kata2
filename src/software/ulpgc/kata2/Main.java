d2286d37723b251c2dece44f8f94106505dbbf18
Jorge Alamo <jorgealamosan@gmail.com>
2023-10-21 13:17:17 +0100

    Class Main

--- src/software/ulpgc/kata2/Main.java
+++ src/software/ulpgc/kata2/Main.java
@@ -1,4 +1,14 @@
 package software.ulpgc.kata2;
 
+import java.io.File;
+import java.util.List;
+
 public class Main {
+    public static void main(String[] args) {
+        FlightLoader loader = new TsvFileFlightLoader(new File("flights.txt"));
+        List<Flight> flights = loader.load();
+        for (Flight flight : flights) {
+            System.out.println(flight.getYear() + ", " + flight.getMonth() + ": " + flight.getPassengers());
+        }
+    }
 }
